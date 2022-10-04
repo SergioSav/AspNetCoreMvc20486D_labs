5e07c7645359c31b4dcb5f1db6273488c4c966ce
SergSav <s.savoskin@mail.ru>
2022-10-04 13:18:01 +0300

    added labs for module 3 day 2 exercise 3 and begin 4

---
+++ Day2/Lab_2d_01/PollBall/PollBall/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PollBall.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello from controller");
+        }
+    }
+}
