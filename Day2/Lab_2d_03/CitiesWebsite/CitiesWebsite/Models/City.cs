e94f8531e2e3c6f5d6847a0d74663f2812532bba
SergSav <s.savoskin@mail.ru>
2022-10-04 23:11:51 +0300

    added labs for module 5 day 2 exercise 1

---
+++ Day2/Lab_2d_03/CitiesWebsite/CitiesWebsite/Models/City.cs
@@ -0,0 +1,19 @@
+﻿namespace CitiesWebsite.Models
+{
+    public class City
+    {
+        public string Country { get; }
+        public string CityName { get; }
+        public string Name { get; }
+        public string TimeZone { get; }
+        public CityPopulation Population { get; }
+
+        public City(string country, string cityName, string timeZone, CityPopulation population)
+        {
+            Country = country;
+            CityName = cityName;
+            TimeZone = timeZone;
+            Population = population;
+        }
+    }
+}
---
+++ Day2/Lab_2d_03/CitiesWebsite/CitiesWebsite/Startup.cs
@@ -0,0 +1,36 @@
+﻿using CitiesWebsite.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CitiesWebsite
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<ICityProvider, CityProvider>();
+            services.AddSingleton<ICityFormatter, CityFormatter>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "Default",
+                template: "{controller}/{action}",
+                defaults: new { controller = "City", action = "ShowCities" });
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Page not found");
+            });
+        }
+    }
+}
