cc7ce807bc51190b85b7ea1013a1d34e2cc1da30
SergSav <s.savoskin@mail.ru>
2022-10-04 18:28:01 +0300

    added labs for module 4 day 2 exercise 1

---
+++ Day2/Lab_2d_02/WorldJourney/WorldJourney/Controllers/CityController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using WorldJourney.Models;
+
+namespace WorldJourney.Controllers
+{
+    public class CityController : Controller
+    {
+        private IData _data;
+        private IHostingEnvironment _environment;
+
+        public CityController(IData data, IHostingEnvironment environment)
+        {
+            _data = data;
+            _environment = environment;
+
+            _data.CityInitializeData();
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Page"] = "Search city";
+            return View();
+        }
+
+        public IActionResult Details(int? id)
+        {
+            ViewData["Page"] = "Selected city";
+            City city = _data.GetCityById(id);
+            if (city == null)
+                return NotFound();
+            ViewBag.Title = city.CityName;
+            return View(city);
+        }
+
+        public IActionResult GetImage(int? cityId)
+        {
+            ViewData["Message"] = "Display image";
+            City requestedCity = _data.GetCityById(cityId);
+            if (requestedCity != null)
+            {
+                var webRootPath = _environment.WebRootPath;
+                var folderPath = "/images/";
+                string fullPath = webRootPath + folderPath + requestedCity.ImageName;
+                FileStream fileOnDisk = new FileStream(fullPath, FileMode.Open);
+                byte[] fileBytes;
+                using (BinaryReader br = new BinaryReader(fileOnDisk))
+                {
+                    fileBytes = br.ReadBytes((int)fileOnDisk.Length);
+                }
+                return File(fileBytes, requestedCity.ImageMimeType);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
