8935d1d3dbb404fde7f9d48c10ba1aeab9b158cd
SergSav <s.savoskin@mail.ru>
2022-10-05 17:07:02 +0300

    added labs for module 7 day 3 exercise 1

---
+++ Day3/Lab_3d_02/Cupcakes/Cupcakes/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Cupcakes.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Cupcakes
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CupcakeContext>(options => options.UseSqlite("Data Source=cupcake.db"));
+            services.AddMvc(opt => opt.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, CupcakeContext cupcakeContext)
+        {
+            cupcakeContext.Database.EnsureDeleted();
+            cupcakeContext.Database.EnsureCreated();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "CupcakeRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Cupcake", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+            });
+        }
+    }
+}
