7ba48e22032d9c673f953ffa3988e785c7f6226c
SergSav <s.savoskin@mail.ru>
2022-10-06 18:06:34 +0300

    added labs for module 8 day 3 all exercises

---
+++ Day3/Lab_3d_03/ZooSite/ZooSite/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ZooSite.Data;
+using ZooSite.Middleware;
+
+namespace ZooSite
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ZooContext>(options =>
+                  options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc(opt=>opt.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ZooContext zooContext)
+        {
+            zooContext.Database.EnsureDeleted();
+            zooContext.Database.EnsureCreated();
+
+            app.UseStaticFiles();
+            app.UseNodeModules(env.ContentRootPath);
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "ZooRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Zoo", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+            });
+        }
+    }
+}
