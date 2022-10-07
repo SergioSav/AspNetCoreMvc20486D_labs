e23aea7fff5d0958f1a19180362f64b450a3cde3
SergSav <s.savoskin@mail.ru>
2022-10-07 12:19:50 +0300

    added labs for module 9 day 4 exercise 1

---
+++ Day4/Lab_4d_01/IceCreamCompany/IceCreamCompany/gulpfile.js
@@ -0,0 +1,37 @@
+﻿var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var paths = {
+    webroot: "./wwwroot/",
+    nodeModules: "./node_modules/"
+}
+
+paths.jqueryjs = paths.nodeModules + "jquery/dist/jquery.js";
+paths.destinationFolder = paths.webroot + "scripts/";
+paths.vendorjsFileName = "vendor.min.js";
+paths.jsFiles = "./Scripts/*.js";
+paths.jsFileName = "script.min.js";
+
+gulp.task("copy-js-file", function() {
+    return gulp.src(paths.jqueryjs)
+        .pipe(gulp.dest(paths.destinationFolder));
+});
+
+gulp.task("min-vendor:js", function () {
+    return gulp.src(path.jqueryjs)
+        .pipe(concat(paths.vendorjsFileName))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.destinationFolder));
+});
+
+gulp.task("min:js", function () {
+    return gulp.src(paths.jsFiles)
+        .pipe(concat(paths.jsFileName))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.destinationFolder));
+});
+
+gulp.task("js-watcher", function () {
+    return gulp.watch(paths.jsFiles, gulp.series("min:js"));
+});
